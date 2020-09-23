0a524400ca032ca5d2d84c7d0956c3ee65d16b65
Samuel Lassoie <samuel.lassoie+1@gmail.com>
2020-09-23 23:00:55 +0200

    ip

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# 1-ere-info
+# Principes algorithmiques et programmation
+
+
+- [X] 1. [Installation de l'environnement de travail](documentation/1.md)
+
+- [X] 2. [Commentaires, création de variables, affectation de variables, les types primitifs de variables …](documentation/2.md)
---
+++ index.php
@@ -0,0 +1,49 @@
+$maPremiereVariable = 'Hello World';<br>
+<!--
+    MON COMMENTAIRE HTML
+    -->
+<?php
+
+// MON COMMENTAIRE
+/* 
+MON DEUXIEME COMMENTAIRE */
+$maPremiereVariable = 'Hello World';
+
+echo $maPremiereVariable;
+echo "<br>";
+
+echo 'Salut c\'est moi - $maPremiereVariable';
+
+// Declaration de variable
+$nbre;
+// Affectation
+$nbre = 10;
+print '<br>';
+print $nbre;
+
+
+// Différents types
+$entier = 10; // integer
+echo "<pre>";
+    var_dump($entier);
+echo "</pre>";
+
+
+$flottant = 10.15; // float
+
+echo "<pre>";
+    var_dump($flottant);
+echo "</pre>";
+
+$chaineCara = 'messages';
+
+echo "<pre>";
+    var_dump($chaineCara);
+echo "</pre>";
+
+
+$bolean = false;
+
+echo "<pre>";
+    var_dump($bolean);
+echo "</pre>";
